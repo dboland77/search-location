9147a97e26111700f7a28c6968a18734f616da64
Dave Boland <dboland77@gmail.com>
2021-06-12 18:03:11 +0100

    react router added home and map components created

---
+++ src/Home.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { Form, ListGroup, Spinner } from "react-bootstrap";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+export default function App() {
+  const baseURL = "https://code-challenge-backend.herokuapp.com/locations";
+
+  const [suggestions, setSuggestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [isNameSelected, setIsNameSelected] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [URL, setURL] = useState(baseURL);
+
+  // can add either /is or ?q=lon
+
+  useEffect(() => {
+    const loadLocations = async () => {
+      const response = await axios.get(URL);
+      setLocations(response.data);
+    };
+    setIsLoading(true);
+    loadLocations();
+    setIsLoading(false);
+  }, [URL]);
+
+  useEffect(() => {
+    let matches = [];
+    if (name.length > 0) {
+      matches = locations.filter((location) => {
+        const regex = new RegExp(`^${name}`, "gi");
+        return location.name.match(regex);
+      });
+    }
+    setSuggestions(matches);
+  }, [locations, name]);
+
+  const handleInputChange = (e) => {
+    let text = e.target.value;
+    setURL(`${baseURL}?q=${text}`);
+    setName(text);
+  };
+
+  const onNameSelected = (selectedName) => {
+    setName(selectedName);
+    setIsNameSelected(true);
+    setSuggestions([]);
+  };
+
+  return (
+    <div className="Home">
+      <Form.Group className="typeahead-form-group">
+        <Form.Control
+          type="text"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={name}
+        />
+        <div> {name}</div>
+        <ListGroup className="typeahead-list-group">
+          {!isNameSelected &&
+            suggestions.length > 0 &&
+            suggestions.map((result) => (
+              <ListGroup.Item
+                key={result.id}
+                className="typeahead-list-group-item"
+                onClick={() => onNameSelected(result.name)}
+              >
+                {result.name}
+              </ListGroup.Item>
+            ))}
+          {!suggestions.length && isLoading && (
+            <div className="typeahead-spinner-container">
+              <Spinner animation="border" />
+            </div>
+          )}
+        </ListGroup>
+      </Form.Group>
+    </div>
+  );
+}
--- src/Map.js
+++ src/Map.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import {GoogleMap, useLoadScript} from '@react-google-maps/api';
-import {mapStyles} from "./mapStyles";
+import React, {Fragment} from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { mapStyles } from "./mapStyles";
+import { Link } from "react-router-dom";
 
 const containerStyle = {
-  width: '100vw',
-  height: '100vh'
-}
+  width: "100vw",
+  height: "100vh",
+};
 
 const center = {
   lat: 35.6804,
   lng: 139.769,
-}
+};
 
 const options = {
   styles: mapStyles,
-}
+};
 
 const Map = () => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
-  if (loadError) return 'Error loading maps';
-  if (!isLoaded) return 'Loading maps';
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading maps";
 
   return (
-    <div className="Map">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        options={options}
-      ></GoogleMap>
-    </div>
+    <Fragment>
+      <div className="Map">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          options={options}
+        ></GoogleMap>
+      </div>
+      <Link to="/">Home</Link>
+    </Fragment>
   );
-}
+};
 
-export default Map
+export default Map;
